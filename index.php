f23ecff50b4e165480083a851f8ddb6ea6d9ef66
EgorDubovik <posik.web.m@gmail.com>
2019-09-14 22:31:29 -0400

    creat and add scripts to project

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Read and learn books</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	
+	<div class="cont_text">
+
+	</div>
+
+	<script type="text/javascript">
+		$(document).ready(function(){
+			add_text_to_page("Meet my family. There are five of us – my parents, my elder brother, my baby sister and me. First, meet my mum and dad, Jane and Michael. My mum enjoys reading and my dad enjoys playing chess with my brother Ken. My mum is slim and rather tall. She has long red hair and big brown eyes. She has a very pleasant");
+		});
+	</script>
+<div id="popup" style="display: none;">
+
+	<div class="information_c">
+		<div class="general_w"></div>
+		<div class="translate_w"></div>
+	</div>
+	<div class="cont_svg">
+		<svg width="30" height="30" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+			<circle cx="50" cy="50" fill="none" stroke="#71c2cc" stroke-width="10" r="35" stroke-dasharray="164.93361431346415 56.97787143782138" transform="rotate(4.22856 50 50)">
+				<animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 50;360 50 50" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animateTransform>
+			</circle>
+		</svg>
+	</div>
+</div>
+<div class="close_popup"></div>
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,86 @@
+var api_key = "trnsl.1.1.20190915T003556Z.59df2ad7aad1df77.c5e6a093b9d0d58d90554dee74a7e386e7ae01a5";
+var url = "https://translate.yandex.net/api/v1.5/tr.json/translate";
+
+function add_text_to_page(text){
+	var text_array = text.split(" ");
+	var new_text = "";
+	for (var i = 0; i <= text_array.length - 1; i++) {
+		new_text +="<span>"+text_array[i]+"</span> ";
+	}
+	$('.cont_text').html(new_text);
+	$('.cont_text span').click(function(event){
+		var w = $(this).text();
+		popup.show(event.clientX,event.clientY);
+		popup.show_loading();
+		translate(w,function(json){
+			console.log(json);
+			popup.add_i(json,w,event.clientX,event.clientY);
+			popup.hide_loading();
+		});
+	});
+	$('.close_popup').click(function(){
+		popup.hide();
+	});
+}
+
+function translate(w,callback){
+
+	var xhr = new XMLHttpRequest(),
+        data = "key="+api_key+"&text="+w+"&lang=en-ru";
+    xhr.open("POST",url,true);
+    xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    xhr.send(data);
+    xhr.onreadystatechange = function() {
+        if (this.readyState==4 && this.status==200) {
+            var res = this.responseText;
+            var json = JSON.parse(res);
+            callback(json);
+        } else {
+        	callback({"code":405});
+        }
+    }
+}	
+
+var popup = {
+	p : $("#popup"),
+	show : function(x,y){
+		$('.close_popup').show();
+		this.center_p(x,y);
+		$("#popup").show();
+	},
+	hide : function(){
+		$('#popup').hide();
+		$('.close_popup').hide();
+	},
+	add_i : function(res,w,x,y){
+		$('.general_w').html(w);
+		if(res.code == 200){
+			$('.translate_w').html(res.text[0]);
+			this.center_p(x,y);
+		}
+	},
+	show_loading : function(){
+		$('.cont_svg').show();
+		$('.information_c').hide();
+	},
+	hide_loading : function(){
+		$('.cont_svg').hide();
+		$('.information_c').show();
+	},
+	center_p : function(x,y){
+		console.log("center");
+		var wi = $('#popup').outerWidth();
+		var hi = $('#popup').outerHeight();
+		var new_left = x-(wi/2);
+		new_left = (new_left>10) ? new_left : 10;
+		var new_top = y-hi;
+		new_top = (new_top>10) ? new_top : 10;
+		$('#popup').css({"left":new_left,"top":new_top});
+	}
+}
+
+function add_popup(x,y,w){
+	var popup = $("#popup");
+	popup.css({"left":x,"top":y}).html(w);
+}
+
