7d80672cef89517f4ce48d761bd1c0f7e07e4a0c
EgorDubovik <posik.web.m@gmail.com>
2019-09-16 20:32:45 -0400

    add css style to popup (position:fixed)

--- listModel.php
+++ listModel.php
@@ -1,6 +1,6 @@
 <?php
 	include_once 'DB.php';
 	$db = DB::getConection();
-	$row = $db->query("SELECT id,title,level FROM books");
+	$row = $db->query("SELECT id,title,level FROM books ORDER BY id DESC");
 	$books = $row->fetchAll();
 ?>
